57be6967a2135eaf9614e3639da953ff931a4067
AntonioV-1DAW <antoniovillarealmoreno.guadalupe@alumnado.fundacionloyola.net>
2026-04-25 18:12:15 +0200

    Add files via upload

---
+++ OrderBy-Like_Agradecer.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM agradecimientos
+ORDER BY fecha_hora DESC;
+
+SELECT *
+FROM agradecimientos
+WHERE mensaje LIKE '%gracias%';
---
+++ OrderBy-Like_Alumnos.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM alumnos
+ORDER BY nombre ASC;
+
+SELECT *
+FROM alumnos
+WHERE nombre LIKE 'A%';
